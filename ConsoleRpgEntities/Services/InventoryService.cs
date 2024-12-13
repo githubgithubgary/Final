9ff682c2ca1276c0456c5f19c4256fedc308324c
G Jansen <151589567+githubgithubgary@users.noreply.github.com>
2024-12-12 20:34:15 -0600

    Initial commit

---
+++ ConsoleRpgEntities/Services/InventoryService.cs
@@ -0,0 +1,39 @@
+﻿using ConsoleRpgEntities.Models.Equipments;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System;
+using ConsoleRpgEntities.Models.Rooms;
+
+namespace ConsoleRpgEntities.Services
+{
+    public class InventoryService
+    {
+        private readonly IOutputService _outputService;
+
+        public InventoryService(IOutputService outputService)
+        {
+            _outputService = outputService;
+        }
+
+        public void ListItems(List<Item> Items)
+        {
+            _outputService.WriteLine("Inventory:");
+            foreach (var item in Items)
+            {
+                _outputService.WriteLine($"{item.Name}");
+            }
+        }
+        public void ClearItems(List<Item> Items)
+        {
+            foreach (var item in Items)
+            {
+                item.RoomId = null;
+            }
+        }
+
+        //public void ClearItems(List<Room> rmtmp)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
---
+++ README.txt
@@ -0,0 +1,2 @@
+
+- When you start the Game if there are no characters you must Add them or Quit?  Game play will not move forward without at least one character.
